ac512ce124455c219175e610f1edced652abd592
enarciso2009 <enarciso2009@gmail.com>
2019-09-14 18:53:56 -0300

    DepartmentForm (dialog) design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,28 @@
 package gui;
 
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -40,8 +50,9 @@ private ObservableList<Department> obsList;
 
 @FXML
 
-public void onBtNewAction() {
-	System.out.println("onBtNewAction");
+public void onBtNewAction(ActionEvent event) {
+	Stage parentStage = Utils.currentStage(event);
+	createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 }
 
 public void setDepartmentService(DepartmentService service) {
@@ -74,4 +85,27 @@ obsList = FXCollections.observableArrayList(list);
 tableViewDepartment.setItems(obsList);
 
 }
+	
+	private void createDialogForm(String absoluteName, Stage parentStage) {   // função para carregar o formulario para o usuario preencher um novo departamento.
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName)); // carregando a view 
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage(); // criando uma nova variavel 
+			dialogStage.setTitle("Enter Department data"); // colocando o titulo da janela 
+			dialogStage.setScene(new Scene(pane)); // para trazer uma nova cena trazendo o pene como raiz 
+			dialogStage.setResizable(false); // false a janela não pode ser redimencionada 
+			dialogStage.initOwner(parentStage); //colocando quem é o pai desta janela
+			dialogStage.initModality(Modality.WINDOW_MODAL); // este comando que fala que o a janela esta travada 
+			dialogStage.showAndWait(); 
+			
+			
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+		
+		
+		
+	}
 }
